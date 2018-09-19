a9445bf5bc393c14f96f8c8a45c3e753ca1b9589
None <fake@fake.com>
2018-09-19 12:36:36 +0100

    Added grunt uglify to Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function ( grunt ) {
                 files:
                 [{
                     expand : true,
-                    cwd : 'game-logic/',
+                    cwd : 'game_logic/',
                     src : '**/*.js',
                     dest: 'game-logic.min.js'
                 }]
