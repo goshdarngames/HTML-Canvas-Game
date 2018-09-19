9016703ef8bc1c974f4e35781b7b7fe88cee7ab9
None <fake@fake.com>
2018-09-19 13:16:45 +0100

    Added concat to default grunt tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,9 @@ module.exports = function ( grunt ) {
 
     grunt.loadNpmTasks ( 'grunt-run' );
     grunt.loadNpmTasks ( 'grunt-contrib-concat' );
+
+    //Note: uglification can be added later as a separate 'build' command.
+    //      The build command would remove tests before minifying code.
     //grunt.loadNpmTasks ( 'grunt-contrib-uglify' );
 
     grunt.initConfig ({
@@ -29,5 +32,5 @@ module.exports = function ( grunt ) {
                  
     });
 
-    grunt.registerTask( 'default', ['run:npm_test_jest']);
+    grunt.registerTask( 'default', ['run:npm_test_jest','concat']);
 };
