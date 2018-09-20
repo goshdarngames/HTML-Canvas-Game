302ebb0ae5383131487419d739b390fa94281347
None <fake@fake.com>
2018-09-20 17:40:37 +0100

    Removed 'jest_verification' test.

--- jest_verification/sum.js
+++
@@ -1,9 +0,0 @@
-// This file contains a sum function that is tested with Jest in order to
-// validate the installation of the testing framework.
-
-
-function sum ( a , b )
-{
-    return a + b;
-}
-module.exports = sum;
