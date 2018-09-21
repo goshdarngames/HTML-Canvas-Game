3883aab182e6379756eeea11d1eb5ada7d06b22b
None <fake@fake.com>
2018-09-21 13:05:50 +0100

    Renamed main to init.

--- game_logic/main.test.js
+++
@@ -1,8 +0,0 @@
-//Testing that this test is not included when concatenating
-
-const main = require ('./main')
-
-test ( 'checks 1+1 = 2', () => 
-        {
-            expect ( 1+1 ).toBe (2);
-        });
