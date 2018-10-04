da82bedf2d20047909bfce1c32bff0293fd781b3
None <fake@fake.com>
2018-10-04 14:24:15 +0100

    Added global function to mock the BABYLON object.

--- game_logic/startScene.test.js
+++ game_logic/startScene.test.js
@@ -1,5 +1,26 @@
 const init = require ( "./startScene" );
 
+/****************************************************************************
+ * MOCK DATA
+ ***************************************************************************/
+
+/**
+ * Creates a mock of the BABYLON object with the functions needed for the
+ * tests.
+ */
+function get_mock_babylon ()
+{
+    babylon = jest.fn ();
+
+    babylon.Scene = jest.fn ();
+
+    return babylon;
+}
+
+/****************************************************************************
+ * TESTS
+ ***************************************************************************/
+
 test ( "window.babylonProject.startScene is defined", () =>    
     {
         expect ( window.babylonProject.startScene ).toBeDefined ();
@@ -7,8 +28,7 @@ test ( "window.babylonProject.startScene is defined", () =>
 
 test ( "window.babylonProject.startScene return value not null", () =>
     {
-        window.babylonProject.BABYLON = jest.fn();
-        window.babylonProject.BABYLON.Scene = jest.fn(); 
+        window.babylonProject.BABYLON = get_mock_babylon();
 
         expect ( window.babylonProject.startScene() ).not.toBeNull ();
     });
