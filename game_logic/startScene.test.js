a03fc433abd24dc3f4569944cfe9f4c9df6ef08b
None <fake@fake.com>
2018-10-04 12:32:10 +0100

    Added startscene not null test.

--- game_logic/startScene.test.js
+++ game_logic/startScene.test.js
@@ -1,7 +1,11 @@
-const init = require ( './startScene' );
+const init = require ( "./startScene" );
 
-test ( 'window.babylonProject.startScene is defined', () =>    
+test ( "window.babylonProject.startScene is defined", () =>    
     {
         expect ( window.babylonProject.startScene ).toBeDefined ();
     });
 
+test ( "window.babylonProject.startScene return value not null", () =>
+    {
+        expect ( window.babylonProject.startScene() ).not.toBeNull ();
+    });
