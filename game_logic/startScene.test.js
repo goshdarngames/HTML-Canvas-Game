8c8813e7d166333f0c4ea710cdd29cf0279448c8
None <fake@fake.com>
2018-10-05 12:16:50 +0100

    Grouped tests using describe block.

--- game_logic/startScene.test.js
+++ game_logic/startScene.test.js
@@ -25,28 +25,30 @@ function get_mock_babylon ()
 /****************************************************************************
  * TESTS
  ***************************************************************************/
-
-test ( "window.babylonProject.startScene is defined", () =>    
-    {
-        expect ( window.babylonProject.startScene ).toBeDefined ();
-    });
-
-test ( "window.babylonProject.startScene return value not null", () =>
-    {
-        window.babylonProject.BABYLON = get_mock_babylon();
-
-        expect ( window.babylonProject.startScene() ).not.toBeNull ();
-    });
-
-test ( "window.babylonProject.startScene.clearColor is Color3", () =>
-    {
-        window.babylonProject.BABYLON = get_mock_babylon();
-
-        var scene = window.babylonProject.startScene();
-        
-        var babylon = window.babylonProject.BABYLON;
-
-        expect ( babylon.Color3 ).toHaveBeenCalledTimes ( 1 );
-        
-        expect ( scene.clearColor ).toBeInstanceOf ( babylon.Color3 );
-    });
+describe ("window.babylonProject.startScene", () =>
+{
+    test ( "is defined", () =>    
+        {
+            expect ( window.babylonProject.startScene ).toBeDefined ();
+        });
+    
+    test ( "is not null", () =>
+        {
+            window.babylonProject.BABYLON = get_mock_babylon();
+    
+            expect ( window.babylonProject.startScene() ).not.toBeNull ();
+        });
+    
+    test ( "has clearColor that is Color3", () =>
+        {
+            window.babylonProject.BABYLON = get_mock_babylon();
+    
+            var scene = window.babylonProject.startScene();
+            
+            var babylon = window.babylonProject.BABYLON;
+    
+            expect ( babylon.Color3 ).toHaveBeenCalledTimes ( 1 );
+            
+            expect ( scene.clearColor ).toBeInstanceOf ( babylon.Color3 );
+        });
+});
